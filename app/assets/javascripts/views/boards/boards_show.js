8d9baff97417f5a673014d376ef506bc1f5e55d8
Grace Do <xgrace@gmail.com>
2014-04-06 16:39:56 -0700

    fixed bug (added listener to lists sync event)

--- app/assets/javascripts/views/boards/boards_show.js
+++ app/assets/javascripts/views/boards/boards_show.js
@@ -6,6 +6,7 @@ Trellino.Views.BoardsShow = Backbone.CompositeView.extend({
     this.members = this.model.members();
     
     this.listenTo(this.model, "sync remove", this.render);
+    this.listenTo(this.lists, "sync", this.render);
     this.listenTo(this.lists, "add", this.addList);
     this.listenTo(this.members, "add", this.addMember);
 
